f2a935e0b8fe86ba158515c3e717f6b65a66a045
joydeep100 <joydeep56053@gmail.com>
2020-04-13 16:10:00 +0530

    updated chrome optional args

--- helpers/base_driver.py
+++ helpers/base_driver.py
@@ -11,15 +11,10 @@ class Browser(SeleniumAPIWrapper):
 			chromeOptions = webdriver.ChromeOptions()
 			chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
 			chromeOptions.add_argument("--no-sandbox")
-			chromeOptions.add_argument("--disable-setuid-sandbox") 
-			chromeOptions.add_argument("--remote-debugging-port=9222")
-			chromeOptions.add_argument("--disable-dev-shm-using")
-			chromeOptions.add_argument("--disable-extensions")
-			chromeOptions.add_argument("--disable-gpu")
-			chromeOptions.add_argument("start-maximized")
-			chromeOptions.add_argument("disable-infobars")
-			chromeOptions.add_argument("--headless")
-
+			#Other optional args 
+			# --disable-setuid-sandbox, --remote-debugging-port=9222,  --disable-dev-shm-using
+			# --disable-extensions, --disable-gpu, start-maximized, disable-infobars, --headless
+	
 			self.driver = webdriver.Chrome(chrome_options=chromeOptions)
 	
 
