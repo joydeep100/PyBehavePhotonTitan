87c195b4206ea02c3f4e11206f3cba4443998fc5
joydeep100 <joydeep56053@gmail.com>
2020-04-24 14:41:30 +0530

    minor refactor

--- features/steps/home_page_steps.py
+++ features/steps/home_page_steps.py
@@ -3,35 +3,35 @@ from common.variables import home_url
 
 @given('I go to the home page')
 def step_impl(context):
-	home_page= HomePage(context)
+	home_page =  HomePage(context)
 	home_page.gotoPage(home_url)
 	   
 @given('I verify if the "{label_text}" page is loaded sucessfully')
 def step_impl(context,label_text):
-	home_page= HomePage(context)
+	home_page =  HomePage(context)
 	home_page.validateHomePageIsLoaded(label_text)
 
 @given('I go to the "{link_name}" link')
 def step_impl(context,link_name):
-	home_page= HomePage(context)
+	home_page =  HomePage(context)
 	home_page.gotoLink(link_name)
 
 @given('I select "{item}" from the list of items')
 def step_impl(context,item):
-	home_page= HomePage(context)
+	home_page =  HomePage(context)
 	home_page.selectItemInSelectableWindown(item)
 
 @given('I check if the item is selected')
 def step_impl(context):
-	home_page= HomePage(context)
+	home_page =  HomePage(context)
 	home_page.checkIfItemIsSelected()
 
 @given('I select the item to be dropped')
 def step_impl(context):
-	home_page= HomePage(context)
+	home_page =  HomePage(context)
 	home_page.dragItemToDroppableArea()
 
 @given('I verify if the item is sucessfully dropped')
 def step_impl(context):
-	home_page= HomePage(context)
+	home_page =  HomePage(context)
 	home_page.checkIfItemIsDropped()
